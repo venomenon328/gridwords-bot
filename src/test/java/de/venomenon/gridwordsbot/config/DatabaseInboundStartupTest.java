19f05c17569ac6c3f479d0848ffe82aecc6a7e51
Tobias Schröder <tobias@venomenon.de>
2026-07-29 14:08:08 +0200

    test: verify canonical publication startup recovery

--- src/test/java/de/venomenon/gridwordsbot/config/DatabaseInboundStartupTest.java
+++ src/test/java/de/venomenon/gridwordsbot/config/DatabaseInboundStartupTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.when;
 
 import de.venomenon.gridwordsbot.adapter.discord.inbound.DiscordInboundListener;
 import de.venomenon.gridwordsbot.application.submission.ConfiguredPlayerSynchronizer;
+import de.venomenon.gridwordsbot.application.canonical.CanonicalGridWordsPublicationService;
 import net.dv8tion.jda.api.JDA;
 import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
@@ -45,6 +46,19 @@ class DatabaseInboundStartupTest {
         verify(listenerProvider).getIfAvailable();
     }
 
+    @Test
+    void resumesCanonicalPublicationsBeforeAttachingTheListener() throws Exception {
+        ConfiguredPlayerSynchronizer synchronizer = mock(ConfiguredPlayerSynchronizer.class);
+        CanonicalGridWordsPublicationService canonical = mock(CanonicalGridWordsPublicationService.class);
+        JDA jda = mock(JDA.class);
+        DiscordInboundListener listener = mock(DiscordInboundListener.class);
+        new DatabaseInboundStartup(synchronizer, provider(jda), provider(listener), provider(canonical))
+                .run(new DefaultApplicationArguments());
+        InOrder order = inOrder(synchronizer, canonical, jda);
+        order.verify(synchronizer).synchronize();
+        order.verify(canonical).resumeOpenPublications();
+        order.verify(jda).addEventListener(listener);
+    }
     @SuppressWarnings("unchecked")
     private <T> ObjectProvider<T> provider(T value) {
         ObjectProvider<T> provider = mock(ObjectProvider.class);
