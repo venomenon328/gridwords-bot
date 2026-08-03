5aebffb8ceae96a9f5b77ce21d6567e0e13d3222
venomenon328 <tobias@venomenon.de>
2026-08-03 23:08:25 +0200

    feat: stage excuse catalog package

---
+++ src/main/java/de/venomenon/gridwordsbot/domain/excuse/ExcuseStyle.java
@@ -0,0 +1,13 @@
+package de.venomenon.gridwordsbot.domain.excuse;
+
+/** Stable editorial style identifiers. */
+public enum ExcuseStyle {
+    TECHNICAL,
+    TACTICAL,
+    BUREAUCRATIC,
+    DRAMATIC,
+    COSMIC,
+    NORTHERN_GERMAN,
+    SPORTING,
+    LEGAL
+}
