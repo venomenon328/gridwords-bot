50935b5fba7b48c5c4e2b3803312ecb7a7b2e7e5
venomenon328 <tobias@venomenon.de>
2026-08-03 23:16:47 +0200

    fix: correct staged architecture rule

--- src/test/java/de/venomenon/gridwordsbot/ArchitectureTest.java
+++ src/test/java/de/venomenon/gridwordsbot/ArchitectureTest.java
@@ -75,7 +75,7 @@ class ArchitectureTest {
     static final ArchRule reportingDomainAndApplicationStayFreeOfJdaJdbcAndJpa = noClasses()
             .that().resideInAnyPackage("..domain.reporting..", "..application.reporting..")
             .should().dependOnClassesThat().resideInAnyPackage(
-                    "net.dv8ution.jda..", "org.springframework.jdbc..", "org.springframework.data..",
+                    "net.dv8tion.jda..", "org.springframework.jdbc..", "org.springframework.data..",
                     "jakarta.persistence..", "javax.persistence..", "org.hibernate..");
 
     @ArchTest
