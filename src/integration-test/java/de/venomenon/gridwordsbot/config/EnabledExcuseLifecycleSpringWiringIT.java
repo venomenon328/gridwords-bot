f5dc6ec51aa85ecd50c7c154f782cf636c8f87c8
venomenon328 <tobias@venomenon.de>
2026-08-04 14:13:22 +0200

    test: verify enabled excuse lifecycle Spring wiring

---
+++ src/integration-test/java/de/venomenon/gridwordsbot/config/EnabledExcuseLifecycleSpringWiringIT.java
@@ -0,0 +1,33 @@
+package de.venomenon.gridwordsbot.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import de.venomenon.gridwordsbot.GridwordsBotApplication;
+import de.venomenon.gridwordsbot.domain.excuse.ExcuseStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@ActiveProfiles("database")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@SpringBootTest(
+        classes = GridwordsBotApplication.class,
+        properties = {
+                "gridwords.discord.enabled=false",
+                "gridwords.discord.guild-id=78003",
+                "gridwords.discord.channel-id=78004",
+                "gridwords.discord.admin-user-ids=78001",
+                "gridwords.excuses.enabled=true",
+                "gridwords.excuses.offer-lifetime=PT15M",
+                "spring.main.web-application-type=none"
+        })
+class EnabledExcuseLifecycleSpringWiringIT extends ExcuseLifecycleSpringWiringSupport {
+
+    @Test
+    void qualifyingResultStoredThroughTheInjectedSubmissionStoreCreatesAnAvailableOffer() {
+        assertThat(storeQualifyingResult()).isEqualTo(ExcuseStatus.AVAILABLE);
+    }
+}
