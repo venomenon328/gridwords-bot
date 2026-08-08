245a3bb1094142541d5c5017ccfc80769e65ecdd
venomenon328 <tobias@venomenon.de>
2026-08-08 17:33:44 +0200

    Make achievement Discord wiring property-driven

--- src/main/java/de/venomenon/gridwordsbot/adapter/discord/achievement/AchievementAnnouncementDiscordConfiguration.java
+++ src/main/java/de/venomenon/gridwordsbot/adapter/discord/achievement/AchievementAnnouncementDiscordConfiguration.java
@@ -2,12 +2,12 @@ package de.venomenon.gridwordsbot.adapter.discord.achievement;
 
 import de.venomenon.gridwordsbot.port.out.AchievementAnnouncementMessageGateway;
 import net.dv8tion.jda.api.JDA;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnBean(JDA.class)
+@ConditionalOnProperty(prefix = "gridwords.discord", name = "enabled", havingValue = "true")
 class AchievementAnnouncementDiscordConfiguration {
     @Bean AchievementAnnouncementMessageGateway achievementAnnouncementMessageGateway(JDA jda) {
         return new JdaAchievementAnnouncementMessageGateway(jda);
