3bc88c34022153650a115e71a9ea840e7e55cbd6
venomenon328 <tobias@venomenon.de>
2026-08-11 22:18:09 +0200

    Wire parser recovery completion

--- src/main/java/de/venomenon/gridwordsbot/config/ParserRecoveryConfiguration.java
+++ src/main/java/de/venomenon/gridwordsbot/config/ParserRecoveryConfiguration.java
@@ -1,9 +1,12 @@
 package de.venomenon.gridwordsbot.config;
 
+import de.venomenon.gridwordsbot.application.canonical.CanonicalGridWordsPublicationService;
+import de.venomenon.gridwordsbot.application.canonical.GridWordsSourceDeletionService;
 import de.venomenon.gridwordsbot.application.submission.InvalidDurationRecoveryService;
 import de.venomenon.gridwordsbot.port.in.InvalidDurationRecoveryUseCase;
 import de.venomenon.gridwordsbot.port.in.ProcessSharedResultUseCase;
 import de.venomenon.gridwordsbot.port.out.ParserRecoveryStore;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,7 +18,19 @@ class ParserRecoveryConfiguration {
     @Bean
     InvalidDurationRecoveryUseCase invalidDurationRecoveryUseCase(
             ParserRecoveryStore recoveryStore,
-            ProcessSharedResultUseCase processor) {
-        return new InvalidDurationRecoveryService(recoveryStore, processor);
+            ProcessSharedResultUseCase processor,
+            ObjectProvider<CanonicalGridWordsPublicationService> canonicalProvider,
+            ObjectProvider<GridWordsSourceDeletionService> deletionProvider) {
+        return new InvalidDurationRecoveryService(recoveryStore, processor, sourceMessageId -> {
+            CanonicalGridWordsPublicationService canonical = canonicalProvider.getIfAvailable();
+            GridWordsSourceDeletionService deletion = deletionProvider.getIfAvailable();
+            if (canonical == null || deletion == null) {
+                return false;
+            }
+            if (!canonical.publishMaintenanceRecovery(sourceMessageId)) {
+                return false;
+            }
+            return deletion.deleteAfterCanonicalPublicationMaintenance(sourceMessageId);
+        });
     }
 }
